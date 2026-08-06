2a53590943499fac82ed455fe040615c51ca8bba
Swarnali Mollick <76970926+itsmeSwarnali@users.noreply.github.com>
2026-08-06 23:07:25 +0200

    Update experience.js

--- data/experience.js
+++ data/experience.js
@@ -1,7 +1,7 @@
 export const experience=[
   {
     period:"Sep 2022 - Sep 2023",
-    role:"Web Developer / Web Programmer",
+    role:"Web Developer",
     organisation:"Appstick, Bangladesh",
     detail:"Developed and maintained responsive, database-backed web applications using Laravel, PHP, MySQL, HTML, CSS and Bootstrap."
   },
