c94d4291370c8ca0c4f24e928ac267d3efeff7a2
Swarnali Mollick <76970926+itsmeSwarnali@users.noreply.github.com>
2026-08-06 23:10:12 +0200

    Update experience.js

--- data/experience.js
+++ data/experience.js
@@ -4,11 +4,5 @@ export const experience=[
     role:"Web Developer",
     organisation:"Appstick, Bangladesh",
     detail:"Developed and maintained responsive, database-backed web applications using Laravel, PHP, MySQL, HTML, CSS and Bootstrap."
-  },
-  {
-    period:"Apr 2021 - Feb 2022",
-    role:"Web Designer Intern",
-    organisation:"The Virtual BD, Bangladesh",
-    detail:"Designed responsive WordPress and Elementor pages and received the Best Web Designer Award for internship performance."
   }
 ];
